482d3605e52c400f488f8007e4b3419cd1d5f21d
awais2o <awaisqamar157@gmail.com>
2024-08-24 16:29:24 +0500

    fix11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}' // Make sure these paths cover all your component files
+    './src/**/*.{js,jsx,ts,tsx,css}' // Make sure these paths cover all your component files
   ],
   theme: {
     extend: {}
